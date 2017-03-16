4566c2d13fce3fa329f0d2332ec756d38a246f1c
cleyber <cleyber65@gmail.com>
2017-03-16 16:37:16 -0500

    Se crea formulario para insertar y se avanza en el rest

--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
-import Vue from 'vue'
-import App from './App.vue'
+Messenger.options = {
+    theme: 'air'
+};
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+   el: '#app',
+   data: {
+      todos: [],
+      users: {
+         nombre: '',
+         correo: '',
+         clave: '',
+         confirmar:''
+      }
+   },
+   created(){
+      var self = this;
+      axios.get('http://localhost:3030/todos')
+         .then(function(response){
+            self.todos = response.data;
+         })
+         .catch(function(error){
+            console.error('Error en getAll', error);
+         });
+   },
+   methods:{
+      addToList(){
+         var self = this;
+         if(self.users.nombre && self.users.correo && self.users.clave && self.users.confirmar){
+            axios.post('http://localhost:3030/todos', self.users)
+               .then(function(response){
+                  Messenger().post("Usuario registrado correctamente");
+               })
+               .catch(function(error){
+                  console.error('Error en addToList', error);
+               });
+
+         }else{
+            Messenger().error("Debe rellenar todos los campos");
+         }
+      }
+   }
 })
